f4c8432a97f7f35d880a7535c39db3bbb3062234
Wildes Sousa <74382948+Sousa-Diin@users.noreply.github.com>
2022-08-18 21:38:26 -0300

    :hammer: Melhorar as estrutura de pastas no componente pages

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-//import React, { useContext } from "react";
+
 import React from "react";
 import { BrowserRouter,Route, Routes } from "react-router-dom";
 //import {Switch, Route, Link} from 'react-router-dom';
